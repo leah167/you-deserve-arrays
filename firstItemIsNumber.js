7ffdd1a3c32d29f9cfeb76173aa4dd4174b07f4b
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-02-14 17:36:55 -0700

    final version 2.0

--- firstItemIsNumber.js
+++ firstItemIsNumber.js
@@ -3,16 +3,18 @@ const prompt = require('prompt-sync')();
 // must make array JSON.parse so it's not seen as a string
 let userArray = JSON.parse(prompt("Enter an array: "));
 
+// establish boolean
 let isFirstANum = false
 
 let firstItem = userArray[0]
 
 if (typeof firstItem === 'number') {
-
+    
+     //    redefine boolean under this condition
     isFirstANum = true
 
-    console.log("The first item is a number.")
+    console.log(isFirstANum)
 
-} else console.log("The first item is not a number")
+} else console.log(isFirstANum)
 
 // the number is not recognized as a number if it is in "quotations".
--- secondCharOfThirdString.js
+++ secondCharOfThirdString.js
@@ -3,10 +3,13 @@ const prompt = require('prompt-sync')();
 // must make array JSON.parse so it's not seen as a string
 let userArray = JSON.parse(prompt("Enter an array: "));
 
+// this variable lets us set conditions to the third item in the array specifically
 let thirdItem = userArray[2];
 
+// this variable lets us get the second character of the third item
 let secondChar = thirdItem[1];
 
+// typeof is how you set type conditions
 if (typeof thirdItem === 'string') {
     
 console.log("The second character in the third item in the array is: " + secondChar);
